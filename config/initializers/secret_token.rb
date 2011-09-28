679edd48ededd951b479b13ca49812bc3499074b
Linda Ruiz <skygal.linda@gmail.com>
2011-09-27 19:19:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkShare::Application.config.secret_token = 'c92944fe7bc26ce6f7c8717af0fdfddd97b3a658179fb7ab7660a27863b88c5118388bbcd2ad0407bf99ef8da269946d5405a5e77df7b568e787bfa2204b2299'
